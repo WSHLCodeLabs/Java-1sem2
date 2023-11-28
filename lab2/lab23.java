ff0d6686cbce491445478a9030b4f23ec3a1ac9a
WSHL-wq <94508068+WSHL-wq@users.noreply.github.com>
2023-11-28 17:07:11 +0700

    Create lab23.java

---
+++ lab2/lab23.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class lab23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double maxScore = Double.MIN_VALUE;
+        String maxScoreStudentID = "";
+
+        while (true) {
+            System.out.print("Enter Student ID (or 'Q' to quit): ");
+            String studentID = scanner.nextLine();
+
+            if (studentID.equalsIgnoreCase("Q")) {
+                break;
+            }
+
+            System.out.print("Enter Score: ");
+            double score = scanner.nextDouble();
+            scanner.nextLine(); // Consume the newline character
+
+            if (score > maxScore) {
+                maxScore = score;
+                maxScoreStudentID = studentID;
+            }
+        }
+
+        if (!maxScoreStudentID.isEmpty()) {
+            System.out.println("Maximum score is " + maxScore + " on " + maxScoreStudentID);
+        } else {
+            System.out.println("No valid scores entered.");
+        }
+
+        scanner.close();
+    }
+}
