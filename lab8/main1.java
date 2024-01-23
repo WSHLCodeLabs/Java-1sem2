3cce5aeb63e00be95e57ba8f81f45fa3ab59cc52
WSHL-wq <94508068+WSHL-wq@users.noreply.github.com>
2024-01-23 16:31:26 +0700

    Create main1.java

---
+++ lab8/main1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lab21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = calculateSum(number);
+        System.out.println("The sum of digits is: " + sum);
+
+        scanner.close();
+    }
+
+    public static int calculateSum(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return num % 10 + calculateSum(num / 10);
+        }
+    }
+}
